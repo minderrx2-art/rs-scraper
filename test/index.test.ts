a754b5a564a03d7f666a6bb0b2172b262b497072
Minvydas <minderrx2@gmail.com>
2025-10-07 11:33:26 +0100

    sets up JEST

---
+++ test/index.test.ts
@@ -0,0 +1,13 @@
+const add = (a: number, b: number) => {
+  return a + b
+}
+
+describe('add_0()', () => {
+  test('adds two numbers correctly', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  test('handles negatives', () => {
+    expect(add(-1, 1)).toBe(0);
+  });
+});
