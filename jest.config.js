c49f7decba69b05990179bf2500c2119ed997c22
Minvydas <minderrx2@gmail.com>
2025-10-09 16:42:33 +0100

    remove remnants of jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true }],
-  },
-  extensionsToTreatAsEsm: ['.ts'],
-};
